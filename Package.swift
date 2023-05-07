12082fa492e7c40c73f8aa6cc829bb44827965d1
Johan Thorell <johan.thorell@protonmail.com>
2023-05-07 08:34:08 +0200

    Add XCTestDynamicOverlay to be able to import AsyncExpectations to the main target

--- Package.swift
+++ Package.swift
@@ -7,21 +7,17 @@ let package = Package(
     name: "AsyncExpectations",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AsyncExpectations",
             targets: ["AsyncExpectations"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AsyncExpectations",
-            dependencies: []),
+            dependencies: [.product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay")]),
         .testTarget(
             name: "AsyncExpectationsTests",
             dependencies: ["AsyncExpectations"]),
